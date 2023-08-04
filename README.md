fd4a0301a9a5a01794bfc7fe71b0ff2cc2ede081
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2023-08-04 13:49:16 +0100

    Handle exceptions, update README.md

--- README.md
+++ README.md
@@ -8,5 +8,40 @@ This repository provides scripts that can be run on the Raspberry Pi that will
 monitor the core temperature and start the fan when the temperature reaches
 a certain threshold.
 
-To use this code, you'll have to install a fan. The full instructions can be
-found on our guide: [Control Your Raspberry Pi Fan (and Temperature) with Python](https://howchoo.com/g/ote2mjkzzta/control-raspberry-pi-fan-temperature-python).
+To use this code, you'll have to install a fan. Instructions can be found in many
+guides online (by everyone with different specificities):
+- [Control Your Raspberry Pi Fan (and Temperature) with Python](https://howchoo.com/g/ote2mjkzzta/control-raspberry-pi-fan-temperature-python).
+- [PWM Regulated Fan Based on CPU Temperature for Raspberry Pi](https://www.instructables.com/PWM-Regulated-Fan-Based-on-CPU-Temperature-for-Ras/)
+- [Circuit for temperature-controlled dual-fan Raspberry Pi case](https://scarff.id.au/blog/2021/circuit-for-temperature-controlled-dual-fan-raspberry-pi-case/)
+
+## Running
+
+First, clone the repo:
+
+    git clone https://github.com/rigon/pi-fan-controller
+
+Then, start the fan controller:
+
+    cd pi-fan-controller
+    ./fancontrol.py
+
+You can run the command with:
+ - `-p` to print the current status in every interval
+ - `-d` to run indefinitely
+
+You might need to install the dependency `RPi.GPIO` in your system (should be available by default):
+
+    sudo apt-get install python3-rpi.gpio
+
+Or with `pip`:
+
+    sudo apt install python3-pip
+    sudo pip3 install -r requirements.txt
+
+## Running as service
+
+Just run the install script:
+
+    ./script/install
+
+This script will start the fan controller as well.
--- fancontrol.py
+++ fancontrol.py
@@ -29,39 +29,45 @@ if __name__ == '__main__':
     daemon = '-d' in sys.argv   # Daemon mode
     debug = '-p' in sys.argv    # Print info every interval
 
-    print('ON_THRESHOLD = {}\'C\nOFF_THRESHOLD = {}\'C\nSLEEP_INTERVAL = {}s\nGPIO_PIN = {}'
+    print("ON_THRESHOLD = {}'C\nOFF_THRESHOLD = {}\'C\nSLEEP_INTERVAL = {}s\nGPIO_PIN = {}"
         .format(ON_THRESHOLD, OFF_THRESHOLD, SLEEP_INTERVAL, GPIO_PIN))
 
     if not debug and daemon:    # Help
-        print('Run with -p to print info every interval')
+        print("Run with -p to print info every interval")
 
     # Validate the on and off thresholds
     if OFF_THRESHOLD >= ON_THRESHOLD:
-        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
+        raise RuntimeError("OFF_THRESHOLD must be less than ON_THRESHOLD")
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(GPIO_PIN, GPIO.OUT)
     GPIO.setwarnings(True)
 
-    while True:
-        temp = get_temp()
-        value = GPIO.input(GPIO_PIN)
+    try:
+        while True:
+            temp = get_temp()
+            value = GPIO.input(GPIO_PIN)
 
-        if debug or not daemon:
-            print('temp: {}\'C, fan: {}'.format(temp, "ON" if value else "OFF"))
+            if debug or not daemon:
+                print("temp: {}'C, fan: {}".format(temp, "ON" if value else "OFF"))
 
-        # Start the fan if the temperature has reached the limit and the fan
-        # isn't already running.
-        # NOTE: `value` returns 1 for "on" and 0 for "off"
-        if temp > ON_THRESHOLD and value == GPIO.LOW:
-            GPIO.output(GPIO_PIN, GPIO.HIGH)  # Turn the fan on
+            # Start the fan if the temperature has reached the limit and the fan
+            # isn't already running.
+            # NOTE: `value` returns 1 for "on" and 0 for "off"
+            if temp > ON_THRESHOLD and value == GPIO.LOW:
+                GPIO.output(GPIO_PIN, GPIO.HIGH)  # Turn the fan on
 
-        # Stop the fan if the fan is running and the temperature has dropped
-        # to 10 degrees below the limit.
-        elif value == GPIO.HIGH and temp < OFF_THRESHOLD:
-            GPIO.output(GPIO_PIN, GPIO.LOW)  # Turn the fan off
+            # Stop the fan if the fan is running and the temperature has dropped
+            # to 10 degrees below the limit.
+            elif value == GPIO.HIGH and temp < OFF_THRESHOLD:
+                GPIO.output(GPIO_PIN, GPIO.LOW)  # Turn the fan off
 
-        if not daemon: break
+            if not daemon: break
 
-        time.sleep(SLEEP_INTERVAL)
+            time.sleep(SLEEP_INTERVAL)
+    # If an exception caught (like keyboard interrupt Ctrl+C), the GPIO is set to 0 and the program exits.
+    except BaseException as e:
+        print("Exception caught, exiting due to {}".format(e))
+        GPIO.cleanup()
+        sys.exit()
